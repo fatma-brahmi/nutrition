3a75883f8b6d1721163b1dfacd216ecb60bd9afe
fatma-brahmi <brahmifatma96@gmail.com>
2021-11-07 16:43:20 +0100

    nutrition

---
+++ client/src/components/header/Header.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import {useSelector} from 'react-redux'
+import axios from 'axios'
+
+
+function Header() {
+    const auth = useSelector(state => state.auth)
+
+    const {user, isLogged} = auth
+
+
+    const handleLogout = async () => {
+        try {
+            await axios.get('/user/logout')
+            localStorage.removeItem('firstLogin')
+            window.location.href = "/";
+        } catch (err) {
+            window.location.href = "/";
+        }
+    }
+
+    const userLink = () => {
+        return <li className="drop-nav">
+            <Link to="#" className="avatar">
+            <img src={user.avatar} alt=""/> {user.name} <i className="fas fa-angle-down"></i>
+            </Link>
+            <ul className="dropdown">
+                <li><Link to="/profile">Profile</Link></li>
+                <li><Link to="/" onClick={handleLogout}>Logout</Link></li>
+            </ul>
+        </li>
+    }
+
+    const transForm = {
+        transform: isLogged ? "translateY(-5px)" : 0
+    }
+
+    return (
+        <header>
+            <div className="logo">
+                <h1><Link to="/"><img src="http://images.squarespace-cdn.com/content/v1/53baa4f3e4b0b0a60335858b/1407534675399-HOY1TVSSGG1J2QSFSF0B/logo-01.png?format=750w" alt="" /></Link></h1>
+            </div>
+
+            <ul style={transForm}>
+              
+                {
+                    isLogged
+                    ? userLink()
+                    :<li><Link to="/login"><i className="fas fa-user"></i> Sign in</Link></li>
+                }
+                
+            </ul>
+        </header>
+    )
+}
+
+export default Header
